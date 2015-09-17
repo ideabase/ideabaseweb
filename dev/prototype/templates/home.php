03afbc9f274bdf0bfc421b9533096b733d9be830
Christopher Hallahan <challahan@gmail.com>
2015-09-17 10:36:41 -0400

    Added CSS reference change

---
+++ dev/prototype/templates/home.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>IdeaBase: Home Page Template</title>
+	    <link rel="stylesheet" href="../../assets/css/main.css" />
+	 </head>
+	     
+    <body>
+        <?php include("../patterns/partials/header.html"); ?>
+    </body>
+
+</html>
