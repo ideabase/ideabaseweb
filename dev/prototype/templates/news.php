1803c9a831ed253678b3acc450a20b908c32d564
Reilly <rhawkins@ideabasekent.com>
2015-10-27 11:46:55 -0400

    Finished partial files, installed contact form

--- dev/prototype/templates/news.php
+++ dev/prototype/templates/news.php
@@ -3,13 +3,17 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>IdeaBase: Home Page Template</title>
+        <title>IdeaBase: News Page Template</title>
 	    <link rel="stylesheet" href="../../assets/css/main.css" />
 	 </head>
 	     
     <body>
         <?php include("../patterns/partials/header.html"); ?>
     </body>
+
+    <body>
+    	<?php include("../patterns/partials/news.html"); ?>
+    </body>	
 
     <body>
         <?php include("../patterns/partials/footer.html"); ?>
