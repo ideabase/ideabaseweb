26263455ef4bb9b60e27383d61db883a4d7c469e
Christopher Hallahan <challahan@gmail.com>
2015-11-10 16:39:30 -0500

    Updated site patterns and introduction page
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/patterns/index.php
+++ dev/prototype/patterns/index.php
@@ -2,15 +2,18 @@
 <title>Pattern Library</title> 
 
 <body> 
-
-	<h1>Pattern Library</h1>  
+	<h1>IdeaBase Pattern Library</h1>
+	<p>View a list of patterns used on the IdeaBase website below.</p>
 	<?php 
 		//loops through files in partial directory
 		foreach (glob("partials/*.html") as $filename)
 		{
+			echo '<section class="pattern">';
     		include $filename;
+    		echo '</section>';
 		}	
 	?>   
 	<link href="../../../prod/css/styles.css" rel="stylesheet" type="text/css" media="screen" />
+	<link href="../../../../../style.css" rel="stylesheet" type="text/css" media="screen" />
 </body> 
 </html>
