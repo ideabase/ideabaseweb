90fd0b90f6e12ef44837a2ba479ed5e956ff41d7
Reilly <rhawkins@ideabasekent.com>
2015-12-03 12:28:22 -0500

    updated DB

--- craft/app/Info.php
+++ craft/app/Info.php
@@ -1,10 +1,10 @@
 <?php
 namespace Craft;
 
-define('CRAFT_VERSION', '2.4');
-define('CRAFT_BUILD', '2702');
-define('CRAFT_SCHEMA_VERSION', '2.4.0');
-define('CRAFT_RELEASE_DATE', '1446071777');
+define('CRAFT_VERSION', '2.5');
+define('CRAFT_BUILD', '2750');
+define('CRAFT_SCHEMA_VERSION', '2.5.11');
+define('CRAFT_RELEASE_DATE', '1448991588');
 define('CRAFT_MIN_BUILD_REQUIRED', '2570');
 define('CRAFT_MIN_BUILD_URL', 'http://download.buildwithcraft.com/craft/2.1/2.1.2570/Craft-2.1.2570.zip');
 define('CRAFT_TRACK', 'stable');
