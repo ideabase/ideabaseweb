475955a97c91432abb3b0cb4c5927af5a6f431c3
Christopher Hallahan <challahan@gmail.com>
2015-12-03 16:33:42 -0500

    Developed the footer open/close behavior
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- prod/js/custom-scripts.js
+++ prod/js/custom-scripts.js
@@ -1,5 +1,4 @@
-// Check for JavaScript Support //
-
+// Show and Hide the Navigation Menu, while also changing the menu text to describe open/closed //
 $( "#navshow" ).click(function() {
   $( "nav" ).toggleClass( "nav--show" );
   $( "#navshow" ).toggleClass( "nav__trigger__link--show" );
@@ -7,4 +6,26 @@ $( "#navshow" ).click(function() {
   var menudefault = "Menu";
   $( "#navshow" ).text(menudefault);
   $( ".nav__trigger__link--show" ).text(menuclose);
+});
+
+// Toggle the contact form when the Get in Touch button is clicked.  Also changes the text of the button to describe open/closed //
+$( "#footer-form-toggle" ).click(function() {
+  $( "#footer-form" ).toggleClass( "form--show" );
+  $( "#footer-form-toggle" ).toggleClass( "contact__trigger__link--show" );
+  var contactclose = "Maybe Later?";
+  var contactdefault = "Get In Touch Today";
+  $( "#footer-form-toggle" ).text(contactdefault);
+  $( ".contact__trigger__link--show" ).text(contactclose);
+});
+
+// Smooth scroll the user down to the contact form, while also opening the contact form and adjusting the text to open/closed //
+$( "#contact-toggle" ).click(function() {
+  $( "#footer-form" ).addClass( "form--show" );
+  $( "#footer-form-toggle" ).addClass( "contact__trigger__link--show" );
+  var contactclose = "Maybe Later?";
+  var contactdefault = "Get In Touch Today";
+  $( "#footer-form-toggle" ).text(contactdefault);
+  $( ".contact__trigger__link--show" ).text(contactclose);
+  $("html, body").animate({ scrollTop: $('#footer-form').offset().top }, 1000);
+  return false;
 });
