3197a8d93c34648ed27f0e458d1d56b349bb0f35
Christopher Hallahan <challahan@gmail.com>
2015-12-03 12:19:15 -0500

    Updated visual patterns and templates for prototype
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/home.php
+++ dev/prototype/templates/home.php
@@ -1,19 +1,5 @@
-<!doctype html>
-<html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>IdeaBase: Home Page Template</title>
-	    <link href="../../../prod/css/styles.css" rel="stylesheet" type="text/css" media="screen" />
-	 </head>
-     <script src="../../../prod/js/modernizr-custom.min.js"></script>
-	     
-    <body>
-        <?php include("../patterns/partials/header.html"); ?>
+<?php include("../patterns/partials/header.html"); ?>
         <?php include("../patterns/partials/hero-slot.html"); ?>
         <?php include("../patterns/partials/services.html"); ?>
-        <?php include("../patterns/partials/footer.html"); ?>
-    </body>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="../../../prod/js/custom-scripts-min.js"></script>
-</html>
+        <?php include("../patterns/partials/projects.html"); ?>
+<?php include("../patterns/partials/footer.html"); ?>
--- dev/prototype/templates/process.php
+++ dev/prototype/templates/process.php
@@ -1,23 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>IdeaBase: Services Page Template</title>
-	    <link rel="stylesheet" href="../../assets/css/main.css" />
-	 </head>
-	     
-    <body>
-        <?php include("../patterns/partials/header.html"); ?>
-    </body>
-
-    <body>
+<?php include("../patterns/partials/header.html"); ?>
     	<?php include("../patterns/partials/services.html"); ?>
         <?php include("../patterns/partials/testimonial.html"); ?>
-    </body>	
-
-    <body>
-        <?php include("../patterns/partials/footer.html"); ?>
-    </body>
-
-</html>
+<?php include("../patterns/partials/footer.html"); ?>
