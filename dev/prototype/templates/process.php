30fb763e52f7aad09c124cdff6cab0f762bd4b28
Christopher Hallahan <challahan@gmail.com>
2015-12-04 17:43:34 -0500

    Started on Process page
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/process.php
+++ dev/prototype/templates/process.php
@@ -2,6 +2,9 @@
 		<?php include("../patterns/partials/interior-hero-slot-process.html"); ?>
     	<?php include("../patterns/partials/introduction.html"); ?>
     	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+        <?php include("../patterns/partials/price.html"); ?>
         <?php include("../patterns/partials/testimonial.html"); ?>
 <?php include("../patterns/partials/footer.html"); ?>
 </div>
--- dev/prototype/templates/projects.php
+++ dev/prototype/templates/projects.php
@@ -1,8 +1,5 @@
 <div class="active-page--projects"><?php include("../patterns/partials/header.html"); ?>
     	<?php include("../patterns/partials/interior-hero-slot-projects.html"); ?>
-    	<?php include("../patterns/partals/project.html"); ?>
-        <?php include("../patterns/partials/awards.html"); ?>
-        <?php include("../patterns/partals/project.html"); ?>
-        <?php include("../patterns/partials/awards.html"); ?>
+    	<p>Content coming soon</p>
 <?php include("../patterns/partials/footer.html"); ?>
 </div>
