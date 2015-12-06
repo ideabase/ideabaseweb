3bf32777a6daf0641a565d35510f978c7e0748d2
Christopher Hallahan <challahan@gmail.com>
2015-12-05 22:01:01 -0500

    Updated hero slots and slight layout tweaks

--- dev/prototype/templates/process.php
+++ dev/prototype/templates/process.php
@@ -1,9 +1,11 @@
 <div class="active-page--process"><?php include("../patterns/partials/header.html"); ?>
 		<?php include("../patterns/partials/interior-hero-slot-process.html"); ?>
     	<?php include("../patterns/partials/introduction.html"); ?>
-    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
-    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
-    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+    	<div class="layout-three-wide">
+	    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+	    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+	    	<?php include("../patterns/partials/service-entry-discover.html"); ?>
+	    </div>
         <?php include("../patterns/partials/price.html"); ?>
         <?php include("../patterns/partials/testimonial.html"); ?>
 <?php include("../patterns/partials/footer.html"); ?>
