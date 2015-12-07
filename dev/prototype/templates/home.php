93972526fefed0cd03748ec2c783a717d6dbe640
Christopher Hallahan <challahan@gmail.com>
2015-12-06 19:28:22 -0500

    Optimizations for performance

--- dev/prototype/templates/home.php
+++ dev/prototype/templates/home.php
@@ -1,4 +1,19 @@
 <?php include("../patterns/partials/header.html"); ?>
+	<style type="text/css">
+		.section-about { 
+			background-image: url(../../assets/img/practice-small.jpg)
+		}
+		@media screen and (min-width: 600px) {
+			.section-about {
+			background-image: url(../../assets/img/practice-medium.jpg)
+			}
+		}
+		@media screen and (min-width: 1000px) {
+			.section-about {
+			background-image: url(../../assets/img/practice.jpg)
+			}
+		}
+	</style>
         <?php include("../patterns/partials/hero-slot.html"); ?>
         <?php include("../patterns/partials/services.html"); ?>
         <?php include("../patterns/partials/projects.html"); ?>
