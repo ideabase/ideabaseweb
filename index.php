0a183656b23174aa3e2aae32409d672124f1483e
Christopher Hallahan <challahan@gmail.com>
2015-12-08 16:55:49 -0500

    Updated project hub and added competitive analysis
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- index.php
+++ index.php
@@ -11,37 +11,42 @@
 			<h1>IdeaBase Website Redesign</h1>  
 		</div>
 		<p class="introduction">A retrospective so far of the progress in the IdeaBase website redesign project.</p>
-		<h2>Business Needs</h2>
-		<p>Sept. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
-		<a class="button button-primary" href="#">Business Needs Summary</a>
+		<h2>Business Needs & Competitive Analysis</h2>
+		<p>Sept. 2015 - We met with stakeholders at IdeaBase to determine key business needs for the website, as well as a working timeline.</p>
+		<a class="button button-primary" href="https://docs.google.com/document/d/1cB0yb2QwCxBicR2G9nSv3vijAXP2_VPOG0TXQ0NJw7M/edit">Business Needs Summary</a>
+		<a class="button button-primary" href="dev/research/IdeaBase Competitive Analysis-2.pdf">Competitive Analysis</a>
 		<hr>
-		<h2>Research</h2>
+		<h2>Interviews</h2>
 		<p>Sept. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
-		<a class="button button-primary" href="https://docs.google.com/document/d/1Xc4SMAk_tpuBCHcE1zD8k4pHhgixDt2oEF4c-fXCI98/edit">Brand Research Summary</a>
+		<a class="button button-primary" href="https://docs.google.com/document/d/1Xc4SMAk_tpuBCHcE1zD8k4pHhgixDt2oEF4c-fXCI98/edit">Interview Summary</a>
+		<hr>
+		<h2>Sketching</h2>
+		<p>We started planning content hierarchy and layouts with sketches of the various templates.</p>
+		<a class="button button-primary" href="#">View Website Sketches (Coming Soon)</a>
 		<hr>
 		<h2>Content Development</h2>
-		<p>Oct. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
+		<p>Oct. 2015 - Our Information Architect worked to write and organize draft content as well as a content style guide, which would later be integrated into the prototype and final website.</p>
 		<a class="button button-primary" href="https://drive.google.com/drive/u/0/folders/0ByszO_oMfCZuZExiWXhSVjU3RXc/">View Content and Style Guide</a>
 		<hr>
 		<h2>Visual Style Guide and Style Tiles</h2>
-		<p>Oct. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
+		<p>Our designers documented our visual styles and came up with a set of colors and typography.  We also developed style tiles for the website that would help determine visual direction (was later turned into HTML/CSS).</p>
 		<a class="button button-primary" href="mockups/IdeaBase_StyleTile.pdf">View Style Tiles</a>
 		<hr>
 		<h2>Static Mockups</h2>
-		<p>Nov. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
-		<a class="button button-primary" href="mockups/IdeaBase_StyleTile.pdf">View Static Mockups</a>
+		<p>We made static mockups of the home page and key components in Illustrator and then integrated them into the website prototype.</p>
+		<a class="button button-primary" href="#">View Static Mockups (Coming Soon)</a>
 		<hr>
 		<h2>HTML/CSS Prototype</h2>
-		<p>Nov. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
+		<p>We developed key components and layouts in a working HTML/CSS prototype, which was in turn integrated into the content management system.</p>
 		<a class="button button-primary" href="dev/prototype/templates/home.php">View Prototype</a>
 		<a class="button button-primary" href="dev/prototype/patterns/index.php">View Pattern Library</a>
 		<hr>
 		<h2>Usability Testing</h2>
-		<p>Nov. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
-		<a class="button button-primary" href="dev/prototype/templates/home.php">View Usability Script</a>
+		<p>Using the HTML/CSS Prototype, we conducted testing every few weeks to determine what was and wasn't working</p>
+		<a class="button button-primary" href="dev/prototype/templates/home.php">Testing Script</a>
 		<hr>
 		<h2>Production Website / Content Management System</h2>
-		<p>Nov. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
+		<p>We planned content types and content modeled our fields and then developed a content management system to house the website.  We chose Craft CMS because of its focus on content structure and template flexibility.</p>
 		<a class="button button-primary" href="prod/">View Production Website</a>
 	</div>
 	<link href="prod/css/styles.css" rel="stylesheet" type="text/css" media="screen" />
