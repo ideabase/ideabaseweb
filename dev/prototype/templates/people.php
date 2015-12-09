f81224e858d6bd43106a38afcffed41a24104e0f
Christopher Hallahan <challahan@gmail.com>
2015-12-09 18:21:37 -0500

    Updated profile CSS
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/people.php
+++ dev/prototype/templates/people.php
@@ -3,14 +3,24 @@
     	<?php include("../patterns/partials/introduction.html"); ?>
     	<?php include("../patterns/partials/supplemental-introduction.html"); ?>
     	<div class="panel-primary-container">
-    	<section class="section-services panel-primary">
+    	<section class="section-people panel-primary-wide">
             <section class="people">
+            <h2>Meet Our Team</h2>
     		<?php include("../patterns/partials/staffprofile.html"); ?>
     		<?php include("../patterns/partials/staffprofile.html"); ?>
     		<?php include("../patterns/partials/staffprofile.html"); ?>
-    		<?php include("../patterns/partials/blog-post-teaser.html"); ?>
-    		<?php include("../patterns/partials/blog-post-teaser.html"); ?>
+            <?php include("../patterns/partials/staffprofile.html"); ?>
+            <?php include("../patterns/partials/staffprofile.html"); ?>
+            <?php include("../patterns/partials/staffprofile.html"); ?>
             </section>
+            <?php include("../patterns/partials/blog-post-teaser.html"); ?>
+            <?php include("../patterns/partials/blog-post-teaser.html"); ?>
+                    <section class="people">
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
+                    </section>
     	</section>
     	</div>
     	<?php include("../patterns/partials/work.html"); ?>
