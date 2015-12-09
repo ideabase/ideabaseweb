9c36f583d140ea2df80772cfabc16a1bcad4f777
Christopher Hallahan <challahan@gmail.com>
2015-12-09 17:39:23 -0500

    Updated profiles and footer
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/people.php
+++ dev/prototype/templates/people.php
@@ -4,11 +4,13 @@
     	<?php include("../patterns/partials/supplemental-introduction.html"); ?>
     	<div class="panel-primary-container">
     	<section class="section-services panel-primary">
+            <section class="people">
     		<?php include("../patterns/partials/staffprofile.html"); ?>
     		<?php include("../patterns/partials/staffprofile.html"); ?>
     		<?php include("../patterns/partials/staffprofile.html"); ?>
     		<?php include("../patterns/partials/blog-post-teaser.html"); ?>
     		<?php include("../patterns/partials/blog-post-teaser.html"); ?>
+            </section>
     	</section>
     	</div>
     	<?php include("../patterns/partials/work.html"); ?>
