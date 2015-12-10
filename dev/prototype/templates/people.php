e26b3c70a789583676c1f2a1b104366a9074a49f
Christopher Hallahan <challahan@gmail.com>
2015-12-09 19:33:59 -0500

    Modificadtions to profiles
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/people.php
+++ dev/prototype/templates/people.php
@@ -1,6 +1,6 @@
 <div class="active-page--people"><?php include("../patterns/partials/header.html"); ?>
     	<?php include("../patterns/partials/interior-hero-slot-people.html"); ?>
-    	<?php include("../patterns/partials/introduction.html"); ?>
+    	<?php include("../patterns/partials/introduction-people.html"); ?>
     	<?php include("../patterns/partials/supplemental-introduction.html"); ?>
     	<div class="panel-primary-container">
     	<section class="section-people panel-primary-wide">
@@ -16,6 +16,8 @@
             <?php include("../patterns/partials/blog-post-teaser.html"); ?>
             <?php include("../patterns/partials/blog-post-teaser.html"); ?>
                     <section class="people">
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
+                    <?php include("../patterns/partials/staffprofile.html"); ?>
                     <?php include("../patterns/partials/staffprofile.html"); ?>
                     <?php include("../patterns/partials/staffprofile.html"); ?>
                     <?php include("../patterns/partials/staffprofile.html"); ?>
