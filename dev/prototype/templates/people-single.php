7edfea1c021b20399c145f1b3119dbbbbab15e54
Christopher Hallahan <challahan@gmail.com>
2016-01-11 17:26:36 -0500

    Added styling for profiles
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

---
+++ dev/prototype/templates/people-single.php
@@ -0,0 +1,4 @@
+<div class="active-page--people"><?php include("../patterns/partials/header.html"); ?>
+<?php include("../patterns/partials/staffprofile-single.html"); ?>
+<?php include("../patterns/partials/footer.html"); ?>
+</div>
