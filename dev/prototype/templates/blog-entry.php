4a325de30d85afce22723c7cf12dcf45f54ace64
Christopher Hallahan <challahan@gmail.com>
2016-01-12 12:25:29 -0500

    Added blog entry template
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

---
+++ dev/prototype/templates/blog-entry.php
@@ -0,0 +1,8 @@
+<?php include("../patterns/partials/header.html"); ?>
+            <?php include("../patterns/partials/blog-post-header.html"); ?>
+        <div class="panel-primary-container">
+            <section class="section-blog-entry panel-primary">
+                <?php include("../patterns/partials/blog-post.html"); ?>
+            </section>
+        </div>
+<?php include("../patterns/partials/footer.html"); ?>
--- dev/prototype/templates/blogs.php
+++ dev/prototype/templates/blogs.php
@@ -1,22 +1,3 @@
-<!doctype html>
-<html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>IdeaBase: Blog Page Template</title>
-	    <link rel="stylesheet" href="../../assets/css/main.css" />
-	 </head>
-	     
-    <body>
-        <?php include("../patterns/partials/header.html"); ?>
-    </body>
-
-    <body>
+<?php include("../patterns/partials/header.html"); ?>
     	<?php include("../patterns/partials/blogs.html"); ?>
-    </body>	
-
-    <body>
         <?php include("../patterns/partials/footer.html"); ?>
-    </body>
-
-</html>
