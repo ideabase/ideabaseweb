4f626b5d22f8d4aeaa6115f8c55623a6cd2d6fbd
Christopher Hallahan <challahan@gmail.com>
2016-01-14 15:04:54 -0500

    Additional basic page style mods
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/ux.php
+++ dev/prototype/templates/ux.php
@@ -4,11 +4,12 @@
         <div class="panel-primary-container">
         <?php include("../patterns/partials/basic-page-text.html"); ?>
         <?php include("../patterns/partials/body-image.html"); ?>
-        <?php include("../patterns/partials/basic-page-panel.html"); ?>
         <?php include("../patterns/partials/body-video.html"); ?>
-        <?php include("../patterns/partials/body-button.html"); ?>
         <?php include("../patterns/partials/basic-page-text2.html"); ?>
+        <?php include("../patterns/partials/body-button.html"); ?>
         <?php include("../patterns/partials/body-image.html"); ?>
+        <?php include("../patterns/partials/basic-page-panel.html"); ?>
+
         <?php include("../patterns/partials/testimonial.html"); ?>
     </div>
 <?php include("../patterns/partials/footer.html"); ?>
