9e2d5cd993eeacb7bcdb64a2a25e4c8e16de8577
Christopher Hallahan <challahan@gmail.com>
2016-01-14 19:18:51 -0500

    Final updates to basic page components
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- dev/prototype/templates/basic-page.php
+++ dev/prototype/templates/basic-page.php
@@ -1,27 +1,34 @@
 <?php include("../patterns/partials/header.html"); ?>
-        <?php include("../patterns/partials/blog-post-header"); ?>
-        <div class="panel-primary-container">
+<?php include("../patterns/partials/inline-tags.html"); ?>
+        <?php include("../patterns/partials/interior-hero-slot-directions.html"); ?>
+        <div class="section-basic-page panel-primary-container">
         <section class="section-basic-page panel-primary">
-        <h1>User Experience Testing Lab</h1>
-        <p class="introduction">Planning to visit? Stop by our Downtown Kent location, conveniently located in Acorn Alley. Feel free to use either of our two entrances: our front entrance is next to 4 Cats Art Studio, and our back entrance is behind Tree City Coffee and beside Flashers Cleaners.  Once you’ve arrived, go up the stairs or take the elevator to the second floor and you’ll find our office.</p>
+        <p class="introduction">Planning to visit? Stop by our Downtown Kent location, conveniently located in Acorn Alley located at 138 E. Main Street in Suite 203.</p>
 
-        <h2>Our Address</h2>
-        <p>IdeaBase</p>
-        <p>138 E. Main Street, Suite 203</p>
-        <p>Kent, Ohio 44240</p>
+        <h3>Once You've Arrived</h3>
 
-        <h2>Parking Information</h2>
-        <p>Parking for IdeaBase visitors is available at the following locations:</p>
+        <p>Feel free to use either of our two entrances.  Once you’ve arrived, go up the stairs or take the elevator to the second floor and you’ll find our office.</p>
         <ul>
-            <li>Metered parking is available on E. Main Street (front entrance) and Erie Street (back entrance).  Meters are free on weekdays after 5 p.m. and all day on weekends</li>
-            <li>Hourly parking is available in the PARTA garage</li>
-            <li>Free parking is available:</li>
-            <li>behind the Kent Stage in two hour increments</li>
-            <li>near the bridge</li>
-            <li>near the Pufferbelly </li>
-            <li>in the large lot behind Panini’s on the weekend and after 5 p.m. only</li>
-            <li> If you’re parking on the Kent State campus, we’re about a 10 minute walk down the Lefton Esplanade</li>
+        <li>Front entrance is next to 4 Cats Art Studio</li>
+        <li>Back entrance is behind Tree City Coffee and beside Flashers Cleaners</li>
         </ul>
+        </section>
+        <?php include("../patterns/partials/body-image.html"); ?>
+        <section class="section-basic-page panel-primary">
+        <h3>Paid Parking</h3>
+        <ul>
+            <li>Metered parking is available on E. Main Street (front entrance) and Erie Street (back entrance).  Meters are free on weekdays after 5 p.m. and all day on weekends.</li>
+            <li>Hourly parking is available in the PARTA parking deck.</li>
+        </ul>
+        <h3>Free Parking</h3>
+        <ul>
+            <li>Behind the Kent Stage in two hour increments</li>
+            <li>Near the bridge</li>
+            <li>Near the Pufferbelly </li>
+            <li>In the large lot behind Panini’s on the weekend and after 5 p.m. only</li>
+        </ul>
+        <p>If you’re parking on the Kent State campus, we’re about a 10 minute walk down the Lefton Esplanade.</p>
+        <p>Need help finding our office?  Give us a call at 330-672-7300.</p>
         </section>
     </div>
 <?php include("../patterns/partials/footer.html"); ?>
--- dev/prototype/templates/work.php
+++ dev/prototype/templates/work.php
@@ -3,16 +3,18 @@
         <?php include("../patterns/partials/interior-hero-slot-work.html"); ?>
         <div class="section-basic-page panel-primary-container">
         <section class="section-basic-page panel-primary">
-
         <p class="introduction">Put Your Design Skills to the Test.  At IdeaBase, we believe that top-performing students should have the opportunity to gain real-world experience before graduating.</p>
+        <h2>Qualifications</h2>
         <p>Our student employees come from a variety of backgrounds and benefit from our comprehensive service offering.  Our student employees are:</p>
         <ul>
         <li>Part-time or full-time Kent State University students</li>
         <li>Taking at least 6 credit hours</li>
         <li>Passionate about design</li>
         </ul>
-        
-        <h2>Degree Programs</h2>
+        </section>
+        <?php include("../patterns/partials/body-image.html"); ?>
+        <section class="section-basic-page panel-primary">
+        <h3>Degree Programs</h3>
         <p>Our students are primarily from the College of Communication and Information, but we have team members from other majors as well.  Some of our past student majors include:</p>
         <ul>
         <li>Visual Communication Design</li>
@@ -26,7 +28,7 @@
         <li>Library & Information Science</li>
         <li>Digital Science</li>
         </ul>
-        <h2>Apply for a Job</h2>
+        <h3>Apply for a Job</h3>
         <p>Think you’ll be a great fit? Email your resume to <a href="#">info@ideabasekent.com</a>.</p>
         <p>Not a student? Not a problem.  You can still get involved by mentoring students, donating supplies or joining our professional advisory board. Contact <a href="#">Kristin Dowling, Business Development Manager, today</a>.</p>
         </section>
