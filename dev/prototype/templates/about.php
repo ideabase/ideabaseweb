460045519487ab4cee92e45a48b1621c1816f57e
Christopher Hallahan <challahan@gmail.com>
2016-01-17 23:56:28 -0500

    add content to about page, changed arrow-right reference

--- dev/prototype/templates/about.php
+++ dev/prototype/templates/about.php
@@ -2,5 +2,9 @@
         <?php include("../patterns/partials/inline-tags.html"); ?>
         <?php include("../patterns/partials/interior-hero-slot-about.html"); ?>
         <div class="section-basic-page panel-primary-container">
+            <?php include("../patterns/partials/about-text.html"); ?>
+            <?php include("../patterns/partials/about-video.html"); ?>
+            <?php include("../patterns/partials/about-text2.html"); ?>
+            <?php include("../patterns/partials/body-image.html"); ?>
         </div>
 <?php include("../patterns/partials/footer.html"); ?>
