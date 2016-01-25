7a61161cf49bc7b2c9c325b281521a5dac882683
Christopher Hallahan <challahan@gmail.com>
2016-01-24 19:05:49 -0500

    Made some modifications to the contact form

--- craft/plugins/contactform/models/ContactFormModel.php
+++ craft/plugins/contactform/models/ContactFormModel.php
@@ -7,8 +7,8 @@ class ContactFormModel extends BaseModel
 	{
 		return array(
 			'fromName'   => array(AttributeType::String, 'label' => 'Your Name'),
-			'fromEmail'  => array(AttributeType::Email,  'required' => true, 'label' => 'Your Email'),
-			'message'    => array(AttributeType::String, 'required' => true, 'label' => 'Message'),
+			'fromEmail'  => array(AttributeType::Email,  'required' => false, 'label' => 'Your Email'),
+			'message'    => array(AttributeType::String, 'required' => false, 'label' => 'Message'),
 			'subject'    => array(AttributeType::String, 'label' => 'Subject'),
 			'attachment' => AttributeType::Mixed,
 		);
