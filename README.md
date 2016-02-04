e7b1a9629be7d41925e37e8255021f52921dc80c
Christopher Hallahan <challahan@gmail.com>
2016-02-04 11:03:58 -0500

    Added new splash page, removed databases, moved redesign to redesign folder
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- README.md
+++
@@ -1,7 +0,0 @@
-# IdeaBase Website (2016 Version)
-
-This repository houses the public codebase for the IdeaBase website.  Please see the Issues tab for current issues and improvements.
-
-**Version 1.0**
-
-Coming January 2016.
--- index.php
+++ index.php
@@ -1,56 +1,37 @@
 <html>  
-<title>IdeaBase Website Redesign</title> 
+<title>Coming Soon: The New IdeaBase Website</title> 
 <head>
-	
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body> 
 	<div id="wrapper">
-		<div id="header">
+		<div id="header" class="panel-primary">
 			<img src="dev/assets/img/ideabaselogo.svg" alt="IdeaBase"/>
-			<h1>IdeaBase Website Redesign</h1>  
+					<h2>New Website Coming Soon</h2> 
+					<div class="countdowncontainer">
+					</div>
+					<p class="introduction">On <strong>Valentine's Day, Feb. 14, 2016</strong>, we'll launch the new IdeaBase website, with an improved look at our services, work and people.</p>
+					<p>138 E. Main Street, Suite 203
+			Kent, OH 44240</p>
+					<p><strong>P:</strong> 330.672.7300</p>
+					<p><strong>E:</strong> <a href="mailto:info@ideabasekent.com">info@ideabasekent.com</a></p>
 		</div>
-		<p class="introduction">A retrospective so far of the progress in the IdeaBase website redesign project.</p>
-		<h2>Business Needs & Competitive Analysis</h2>
-		<p>Sept. 2015 - We met with stakeholders at IdeaBase to determine key business needs for the website, as well as a working timeline.</p>
-		<a class="button button-primary" href="https://docs.google.com/document/d/1cB0yb2QwCxBicR2G9nSv3vijAXP2_VPOG0TXQ0NJw7M/edit">Business Needs Summary</a>
-		<a class="button button-primary" href="dev/research/IdeaBase Competitive Analysis-2.pdf">Competitive Analysis</a>
-		<hr>
-		<h2>Interviews</h2>
-		<p>Sept. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
-		<a class="button button-primary" href="https://docs.google.com/document/d/1Xc4SMAk_tpuBCHcE1zD8k4pHhgixDt2oEF4c-fXCI98/edit">Interview Summary</a>
-		<hr>
-		<h2>Sketching</h2>
-		<p>We started planning content hierarchy and layouts with sketches of the various templates.</p>
-		<a class="button button-primary" href="#">View Website Sketches (Coming Soon)</a>
-		<hr>
-		<h2>Content Development</h2>
-		<p>Oct. 2015 - Our Information Architect worked to write and organize draft content as well as a content style guide, which would later be integrated into the prototype and final website.</p>
-		<a class="button button-primary" href="https://drive.google.com/drive/u/0/folders/0ByszO_oMfCZuZExiWXhSVjU3RXc/">View Content and Style Guide</a>
-		<hr>
-		<h2>Visual Style Guide and Style Tiles</h2>
-		<p>Our designers documented our visual styles and came up with a set of colors and typography.  We also developed style tiles for the website that would help determine visual direction (was later turned into HTML/CSS).</p>
-		<a class="button button-primary" href="mockups/IdeaBase_StyleTile.pdf">View Style Tiles</a>
-		<hr>
-		<h2>Static Mockups</h2>
-		<p>We made static mockups of the home page and key components in Illustrator and then integrated them into the website prototype.</p>
-		<a class="button button-primary" href="#">View Static Mockups (Coming Soon)</a>
-		<hr>
-		<h2>HTML/CSS Prototype</h2>
-		<p>We developed key components and layouts in a working HTML/CSS prototype, which was in turn integrated into the content management system.</p>
-		<a class="button button-primary" href="dev/prototype/templates/home.php">View Prototype</a>
-		<a class="button button-primary" href="dev/prototype/patterns/index.php">View Pattern Library</a>
-		<hr>
-		<h2>Usability Testing</h2>
-		<p>Using the HTML/CSS Prototype, we conducted testing every few weeks to determine what was and wasn't working</p>
-		<a class="button button-primary" href="dev/prototype/templates/home.php">Testing Script</a>
-		<hr>
-		<h2>Production Website / Content Management System</h2>
-		<p>We planned content types and content modeled our fields and then developed a content management system to house the website.  We chose Craft CMS because of its focus on content structure and template flexibility.</p>
-		<a class="button button-primary" href="prod/">View Production Website</a>
 	</div>
 	<link href="prod/css/styles.css" rel="stylesheet" type="text/css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="style.css">
-
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="jquery.countdown.min.js"></script>
+	<script type="text/javascript">
+	$(function() {
+	    $('.countdowncontainer').countdown({
+	        date: "February 14, 2016",
+	        render: function(data) {
+	          $(this.el).html("<div>" + this.leadingZeros(data.days, 1) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 1) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 1) + " <span>sec</span></div>");
+	        }
+	    });
+	});
+	</script>
 </body> 
 </html>
