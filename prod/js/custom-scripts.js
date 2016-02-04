3001261be66544a3bb91e4aa8c36668400cc691e
Christopher Hallahan <challahan@gmail.com>
2016-02-04 15:28:24 -0500

    Updates to Projects featured items
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

--- prod/js/custom-scripts.js
+++ prod/js/custom-scripts.js
@@ -63,3 +63,4 @@ sr.reveal('.section-people__logos', { duration: 500, scale: 1  } );
 sr.reveal('.section-services__list__item__icon', { delay: 100, duration: 500 } );
 sr.reveal('.section-header__title', { delay: 100, scale: 1, duration: 500 } );
 sr.reveal('.section-projects__list', { scale: 1, duration: 500 } );
+sr.reveal('.project-list__item--featured', { scale: 1, duration: 800 } );
