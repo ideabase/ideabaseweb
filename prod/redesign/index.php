6e32010edce9974b16bfb18f1c8c9a16b6ddc4b0
Christopher Hallahan <challahan@gmail.com>
2016-04-29 15:12:59 -0400

    Refined and updated the redesign retrospective portion of the website
    Signed-off-by: Christopher Hallahan <challahan@gmail.com>

---
+++ prod/redesign/index.php
@@ -0,0 +1,61 @@
+<html>  
+<title>IdeaBase Website Redesign</title> 
+<head>
+	
+</head>
+
+<body> 
+	<div id="wrapper">
+		<div id="header">
+			<img src="../../dev/assets/img/ideabaselogo.svg" alt="IdeaBase"/>
+			<h1>IdeaBase Website Redesign</h1>  
+		</div>
+		<p class="introduction">A retrospective on the process for the IdeaBase website redesign project, completed in Feb. 2016.</p>
+		<h2>Business Needs & Competitive Analysis</h2>
+		<p>Sept. 2015 - We met with stakeholders at IdeaBase to determine key business needs for the website, as well as a working timeline.</p>
+		<a class="button button-primary" href="../../dev/research/business-needs.pdf">Business Needs Summary</a>
+		<a class="button button-primary" href="../../dev/research/competitive.pdf">Competitive Analysis</a>
+		<hr>
+		<h2>Interviews</h2>
+		<p>Sept. 2015 - We conducted interviews with past clients and students to determine the top tasks for the website, as well as perception of our brand so far.</p>
+		<a class="button button-primary" href="../../dev/research/brand-research.pdf">Interview Summary</a>
+		<hr>
+		<h2>Sketching</h2>
+		<p>Oct. 2015 - We started planning content hierarchy and layouts with sketches and static wireframes of the various templates.</p>
+		<a class="button button-primary" href="../../mockups/sketches.pdf">View Website Sketches</a>
+		<a class="button button-primary" href="../../mockups/wireframes.pdf">View Website Wireframes</a>
+		<hr>
+		<h2>Content Development</h2>
+		<p>Oct. 2015 - Our Information Architect worked to write and organize draft content as well as a content style guide, which would later be integrated into the prototype and final website.</p>
+		<a class="button button-primary" href="../../dev/research/content.zip">View Content Planning Documents</a>
+		<hr>
+		<h2>Visual Style Guide and Style Tiles</h2>
+		<p>Nov. 2015 - Our designers documented our visual styles and came up with a set of colors and typography.  We also developed style tiles for the website that would help determine visual direction (was later turned into HTML/CSS).</p>
+		<a class="button button-primary" href="../../mockups/IdeaBase_StyleTile.pdf">View Static Style Tiles</a>
+		<hr>
+		<h2>Static Mockups</h2>
+		<p>Dec. 2015 - We made static mockups of the home page and key components in Illustrator and then integrated them into the website prototype.</p>
+		<a class="button button-primary" href="../../mockups/mockups.pdf">View Static Mockups</a>
+		<hr>
+		<h2>HTML/CSS Prototype</h2>
+		<p>Dec. 2015 - We developed key components and layouts in a working HTML/CSS prototype, which was in turn integrated into the content management system.</p>
+		<a class="button button-primary" href="../../dev/prototype/templates/home.php">View Prototype</a>
+		<hr>
+		<h2>Usability Testing</h2>
+		<p>Jan. 2016 - Using the HTML/CSS Prototype, we conducted testing every few weeks to determine what was and wasn't working</p>
+		<a class="button button-primary" href="../../dev/research/tasks.pdf">Testing Script/Tasks</a>
+		<hr>
+		<h2>Production Website / Content Management System</h2>
+		<p>Feb. 2016 - We planned content types and content modeled our fields and then developed a content management system to house the website.  We chose Craft CMS because of its focus on content structure and template flexibility.</p>
+		<a class="button button-primary" href="../../dev/sitemap/content-types.pdf">View Content Types</a>
+		<a class="button button-primary" href="http://www.ideabasekent.com">View Production Website</a>
+		<hr>
+		<h2>Visual & Content Style Guide</h2>
+		<p>March 2016 - After the website was launched, we got to work documenting the various content, colors, icons, typefaces and styles used to create the website.  We documented them in an online style guide, accessible to any future IdeaBase staff member who works on communications materials.</p>
+		<a class="button button-primary" href="http://www.ideabasekent.com/style">View Style Guide</a>
+	</div>
+	<link href="../css/styles.css" rel="stylesheet" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</body> 
+</html>
