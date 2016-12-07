7e6fa8c574c2869a7a45a797a796a886a4eab67a
Nate Druss <ndruss@kent.edu>
2016-12-07 13:24:14 -0500

    Reset category template, only load section

--- prod/js/custom-scripts.js
+++ prod/js/custom-scripts.js
@@ -36,15 +36,6 @@ $(".overlay-window").click(function(e){
   e.stopPropagation(); // ...but not when click inside the content area
 })
 
-$(document).ready(function() {
-  var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-  $(".category-link").each(function() {
-    if (this.href === path) {
-      $(this).addClass("current");
-    }
-  });
-});
-
 
 // Toggle the contact form when the Get in Touch button is clicked.  Also changes the text of the button to describe open/closed //
 $( "#footer-form-toggle" ).click(function() {
@@ -132,12 +123,35 @@ $(function() {
 
 $(".category-link").click(function(e){
 
+  // Check if the browser supports HTML5 history
+  var historySupport = !!(window.history && history.pushState);
+
   e.preventDefault();
   var $filter = $(e.currentTarget);
   var href = $filter.attr('href');
 
-  $.ajax({url: href, success: function(result){
-    $(".project-list").html(result);
-  }});
+  if (historySupport) {
+    // Update the browser's address bar
+    history.pushState(null, null, href);  
+  }
+
+  $.ajax({
+    url: href,
+    success: function(result) {
+      $(".section-projects-all").html($(result).find(".project-list"));
+    }
+  });
+
+  var path = window.location.href;
+  if (this.href === path) {
+    $(this).addClass("current");
+    $(this).parent().siblings().find(".category-link").removeClass("current")
+  }
+
+  $(".overlay-window, .overlay").fadeOut(250, "easeOutSine");
+  $("html").removeClass("no-scroll-mobile");
+
+  var category = $(".category__title").text();
+  $(".section-header__title span").html(":" + category);
 
 })
