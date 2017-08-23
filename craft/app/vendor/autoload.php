97ee7e434d417f982ccc95284be572b3518b8990
Christopher Hallahan <challahan@gmail.com>
2017-08-23 10:03:43 -0400

    Update Craft and added aside attribution to RSS Feed

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit044fd0b4692320a10a28f9e509303afb::getLoader();
+return ComposerAutoloaderInit3713697e7d74f82a20dc5e5ceb361b2f::getLoader();
--- craft/app/vendor/composer/autoload_static.php
+++ craft/app/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit044fd0b4692320a10a28f9e509303afb
+class ComposerStaticInit3713697e7d74f82a20dc5e5ceb361b2f
 {
     public static $prefixLengthsPsr4 = array (
         'l' => 
@@ -692,10 +692,10 @@ class ComposerStaticInit044fd0b4692320a10a28f9e509303afb
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit044fd0b4692320a10a28f9e509303afb::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit044fd0b4692320a10a28f9e509303afb::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit044fd0b4692320a10a28f9e509303afb::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit044fd0b4692320a10a28f9e509303afb::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3713697e7d74f82a20dc5e5ceb361b2f::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3713697e7d74f82a20dc5e5ceb361b2f::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit3713697e7d74f82a20dc5e5ceb361b2f::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit3713697e7d74f82a20dc5e5ceb361b2f::$classMap;
 
         }, null, ClassLoader::class);
     }
